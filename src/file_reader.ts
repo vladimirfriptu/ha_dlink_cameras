0a5035533bd32592ae1b51c604215159c23fe36b
vladimirfriptu <v.friptu@andersenlab.com>
2021-04-20 11:20:45 +0300

    Рефакторинг * Чистка кода * Переписал логику получениея sensor_id из названия папки * Подрефачил файл опций

--- src/file_reader.ts
+++ src/file_reader.ts
@@ -2,20 +2,14 @@ import * as fs from "fs";
 import { motionDetect, soundDetect } from "./sensors";
 import * as utils from "./utils";
 
-import options from "./options";
-
 export function readFile(path: string, sensorId: string): void {
   utils.log("trigger " + sensorId + " event");
 
-  switch (sensorId) {
-    case options.sensors.motion:
-      motionDetect(sensorId);
-      break;
-    case options.sensors.sound:
-      soundDetect(sensorId);
-      break;
-    default:
-      console.log("Неизвестное id сенсора. sensorId: " + sensorId);
+  if (sensorId.includes("motion")) motionDetect(sensorId);
+  else if (sensorId.includes("sound")) soundDetect(sensorId);
+  else {
+    console.log("Неизвестное id сенсора. sensorId: " + sensorId);
+    return;
   }
 
   fs.unlink(path, function (err) {
--- src/options.ts
+++ src/options.ts
@@ -2,12 +2,7 @@ import fs from "fs";
 
 interface Options {
   debug: boolean;
-  isSwaggerEnabled: boolean;
   logs_directory_path: string;
-  sensors: {
-    motion: string;
-    sound: string;
-  };
   sensor_live_delay: {
     motion: number;
     sound: number;
